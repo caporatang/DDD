9eeeca1ac4a800e7ad9e828717571bfa3a413c5d
caporatang <caporatang@naver.com>
2025-08-10 21:12:05 +0900

    git init & LearningMakingDDD 프로젝트 생성

---
+++ LearningMakingDDD/src/main/kotlin/org/example/learningmakingddd/LearningMakingDddApplication.kt
@@ -0,0 +1,11 @@
+package org.example.learningmakingddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningMakingDddApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningMakingDddApplication>(*args)
+}
---
+++ LearningMakingDDD/src/test/kotlin/org/example/learningmakingddd/LearningMakingDddApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.learningmakingddd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningMakingDddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
