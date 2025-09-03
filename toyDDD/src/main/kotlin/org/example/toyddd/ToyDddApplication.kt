f1c89aad87973307041131244bb82bcb9dac50b0
caporatang <caporatang@naver.com>
2025-09-03 21:27:20 +0900

    toyDDD init

---
+++ toyDDD/src/main/kotlin/org/example/toyddd/ToyDddApplication.kt
@@ -0,0 +1,11 @@
+package org.example.toyddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyDddApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToyDddApplication>(*args)
+}
---
+++ toyDDD/src/test/kotlin/org/example/toyddd/ToyDddApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.toyddd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyDddApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
